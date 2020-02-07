8cf1e964388b7904b3a89bfe439de143d22c4928
Andy Li <andyli3912@gmail.com>
2020-02-06 21:20:48 -0500

    Fix script to use nvm instead of pacman for installation process

--- scripts/lib/install_node.sh
+++ scripts/lib/install_node.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
-#
 # Install Node and necessary frameworks/packages
 
 # Install Node.js on Linux as root on RHEL, CentOS, CloudLinux, or Fedora
 install_node() {
-  command -v node >/dev/null 2>&1 || { sudo pacman -S nodejs npm };
+  command -v node >/dev/null 2>&1 || {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+    source ~/.nvm.sh
+    nvm install node
+  };
 }
 
 # Install dependencies for notification folder
