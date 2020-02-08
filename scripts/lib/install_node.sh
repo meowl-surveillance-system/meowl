36e3489b31f3c6342b443a454c9de72c0fdeed3a
Andy Li <43677268+Akasora39@users.noreply.github.com>
2020-02-07 19:51:14 -0500

    Create install scripts for node and use nodemailer for notifications (#6)

---
+++ scripts/lib/install_node.sh
@@ -0,0 +1,21 @@
+# Install Node and necessary frameworks/packages
+
+# Install Node.js on Arch Linux
+install_node() {
+  command -v node >/dev/null 2>&1 || {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+    source ~/.nvm/nvm.sh
+    nvm install 12.15.0
+  };
+}
+
+# Install dependencies for notification folder
+init_notif() {
+  cd ../src/notifications && npm install && cd -
+}
+
+#Install node and initializes configuration for Node related apps
+install_node_all() {
+  install_node;
+  init_notif;
+}
