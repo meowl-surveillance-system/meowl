94a03504dc26b160e21493634bd49ef0f8cd36ef
Chieh-Huang Chen <chc9511@gmail.com>
2020-02-15 03:20:39 -0500

    Add comments to script

--- scripts/lib/install_raspberry_pi.sh
+++ scripts/lib/install_raspberry_pi.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
 
+# Installs Python, venv and its requirements on Raspberry Pi
 install_raspberry_pi(){
   sudo apt-get -y update
   sudo apt-get install python3-pip
   sudo apt-get install python3-venv
-  cd ../../src/camera/pi
+  cd ${PARENT_PATH}/../../src/camera/pi
   python3 -m venv venv
+  source ./venv/bin/activate
   pip install -r requirements.txt
 }
