3b8f45c8fb9f5be4528b72b334b26575d98737c9
Chieh-Huang Chen <chc9511@gmail.com>
2020-02-23 15:32:17 -0500

    Add ffmpeg in install script

--- scripts/lib/install_raspberry_pi.sh
+++ scripts/lib/install_raspberry_pi.sh
@@ -3,6 +3,8 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
 # Installs Python, venv and its requirements on Raspberry Pi
 install_raspberry_pi(){
   sudo apt-get -y update
+  sudo apt-get -y upgrade
+  sudo apt-get install ffmpeg
   sudo apt-get install python3-pip
   sudo apt-get install python3-venv
   cd ${PARENT_PATH}/../../src/camera/pi
