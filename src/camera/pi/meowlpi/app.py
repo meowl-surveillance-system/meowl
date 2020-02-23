009c61fea698da887f61ae023b5fa04dafe3ea94
Chieh-Huang Chen <chc9511@gmail.com>
2020-02-23 18:04:30 -0500

    Set up the Raspberry Pi Camera for live-streaming (#19)

---
+++ src/camera/pi/meowlpi/app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, session, current_app
+from meowlpi.camera.camera import PiStreamingCamera
+
+def create_app(test_config=None):
+    """ Create and attach the camera to the Meowl-Pi app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.pi_streaming_camera = PiStreamingCamera
+    if not os.environ.get('RASPBERRY_PI_FLASK_SECRET_KEY'):
+        print('RASPBERRY_PI_FLASK_SECRET_KEY: not found')
+        return None
+    else:
+        app.secret_key = os.environ.get('RASPBERRY_PI_FLASK_SECRET_KEY')
+
+    @app.route('/')
+    def index():
+        """Returns Hello World as a string"""
+        return 'Hello world'
+
+    @app.route('/camera/start')
+    def start_camera_stream():
+        """Starts streaming the camera"""
+        return current_app.pi_streaming_camera.start()
+
+    @app.route('/camera/stop')
+    def stop_camera_stream():
+        """Stops the camera from streaming"""
+        return current_app.pi_streaming_camera.stop()
+    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        app.run(debug=True, host='0.0.0.0')
---
+++ src/camera/pi/tests/test_picamera.py
@@ -0,0 +1,34 @@
+import mock
+import pytest
+from pytest_mock import mocker
+from picamera import PiCamera
+from meowlpi.camera.camera import PiStreamingCamera
+from meowlpi.camera.streamer import MeowlPiStreamer
+
+def test_picamera_recording(mocker):
+    picamera_mock = mocker.Mock()
+    picamera_mock.start_recording.return_value = mocker.Mock()
+    picamera_mock.stop_recording.return_value = mocker.Mock()
+    picamera_mock.close.return_value = mocker.Mock()
+    picamera_mock.closed.__bool__= mocker.Mock(return_value=False)
+    endpoint_stub = mocker.stub(name="endpoint_stub")
+    
+    PiStreamingCamera.camera = picamera_mock
+    PiStreamingCamera.endpoint = endpoint_stub
+    PiStreamingCamera.start()
+    picamera_mock.start_recording.assert_called_with(endpoint_stub, 'h264')
+    PiStreamingCamera.stop()
+    picamera_mock.stop_recording.assert_called_with()
+    picamera_mock.close.assert_called_with()
+
+def test_picamera_not_recording(mocker):
+    picamera_mock = mocker.Mock()
+    picamera_mock.closed.__bool__= mocker.Mock(return_value=True)
+    picamera_mock.stop_recording.return_value = mocker.Mock()
+    picamera_mock.close.return_value = mocker.Mock()
+
+    PiStreamingCamera.camera = picamera_mock
+    PiStreamingCamera.stop()
+
+    picamera_mock.stop_recording.assert_not_called()
+    picamera_mock.close.assert_not_called()
