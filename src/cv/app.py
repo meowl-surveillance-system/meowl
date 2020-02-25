cf51a789610411c54be74a8c9387429b262ff9a3
MicLi01 <Michael.Li68330@gmail.com>
2020-02-24 22:46:12 -0500

    Route to display frames from a stream

--- src/cv/app.py
+++ src/cv/app.py
@@ -1,40 +1,13 @@
 from flask import Flask, request
 import cv2
 import numpy as np
-import yolo_video_detect as obj_detector
+import server.yolo_video_detect as obj_detector
+import server.stream as stream
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-def display_vid(file_name):
-    cap = cv2.VideoCapture(file_name)
-    if (cap.isOpened()== False): 
-        print("OpenCV failed to open video stream or file")
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret == True:
-            cv2.imshow('Frame',frame)
-
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else: 
-            break
- 
-    cap.release()
- 
-    cv2.destroyAllWindows()
-
-    print("Finished Displaying Video")
-
-@app.route('/process/')
-def process_video():
-    args = {
-        "input": 'car_chase_01.mp4',
-        "output": 'car_chase_01.avi',
-        "yolo": 'yolo-coco',
-        "confidence": .5,
-        "threshold": .3
-    }
-    obj_detector.run_object_detection(args)
+@app.route('/display_stream/')
+def display_stream():
+    link = request.args.get('link')
+    stream.get_stream(link)
+    return "Finished Displaying" 
