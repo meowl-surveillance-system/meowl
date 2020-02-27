ff94a1c195f5ea33ede36eafd89c75cc5b5246c1
Chieh-Huang Chen <chc9511@gmail.com>
2020-02-27 15:47:25 -0500

    Modify /api/getVideo endpoint

--- src/web/server/index.ts
+++ src/web/server/index.ts
@@ -1,9 +1,17 @@
 // TODO: Set up the application so that cors is only used for development
 import cors from 'cors';
 import express from 'express';
+const cassandra = require('cassandra')
+
 
 const app = express();
 const port = process.env.PORT || 8081;
+
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'streams'
+});
 
 // TODO: Set up the app so that cors is only used for development
 app.use(cors());
@@ -12,15 +20,10 @@ app.get('/', function(req, res) {
 });
 
 // TODO(Akasora39): Fix the buffer stream
-app.get('/api/getVideo', function(req, res) {
-  const spawn = require('child_process').spawn;
-  const pyProc = spawn('python', ['./sendVid.py'])
-  pyProc.stdout.pipe(res, {end: true});
-  pyProc.stdin.on('end', function() {
-    console.log('done');
-    res.send();
-    return res.end();
-  });
+app.get('/api/getVideo/:streamId', function(req, res) {
+ const selectChunkId = 'SELECT chunk_id FROM metadata WHERE stream_id = ?';
+ client.execute(selectChunkId, [ req.params.streamId ])
+   .then(result => console.log(result.rows[0]))
 })
 
 app.listen(port, function() {
