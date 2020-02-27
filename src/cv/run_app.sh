94590c2b644a4986ab1ddd53928f588325ca7057
MicLi01 <Michael.Li68330@gmail.com>
2020-02-26 22:24:17 -0500

    added tests and address pr comments

--- src/cv/run_app.sh
+++ src/cv/run_app.sh
@@ -1,4 +1,14 @@
 APP_NAME='app'
+YOLO_NAMES='coco.names'
+YOLO_CONFIG='yolov3.cfg'
+YOLO_WEIGHTS='yolov3.weights'
+
+PARENT=$(pwd)
+YOLO='yolo-coco'
+export YOLO_NAMES_PATH=${PARENT}/${YOLO}/${YOLO_NAMES}
+export YOLO_CONFIG_PATH=${PARENT}/${YOLO}/${YOLO_CONFIG}
+export YOLO_WEIGHTS_PATH=${PARENT}/${YOLO}/${YOLO_WEIGHTS}
+
 
 # Run Opencv Server
 source venv/bin/activate
