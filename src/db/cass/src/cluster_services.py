0391637feb331fbcb5e1b013d5b764e5cfc5b14a
Yufeng Liu <yliu5683@gmail.com>
2020-03-02 23:23:28 -0500

    Add services for multimedia storage (#17)
    * Add python driver connection script and file iterator script
    
    * Add video storage and video retrieval functionalities in Cassandra
    
    * Rename connect.py to cluster_services.py and add docs to files
    
    * Use file_id for retrieval of file
    
    * Remove FileIterator class, generate a randomId as file_id and use f.read()
    
    * Refactor ClusterServices and add cassandra-driver installation
    
    * Modified docstring
    
    * Install cassandra-driver in venv
    
    * Change session to private variable
    
    * Add RTMP server connection
    
    * Add RtmpConnection class and update requirements
    
    * Add function to stop reading stream
    
    * Remove app.py
    
    * Add yes flags
    
    * Raise exception on invalid keyspace name
    
    * Update requirements
    
    * Add stunnel conf file and add pip upgrade
    
    * Make threading changes
    
    * Fix spelling mistake
    
    * Import datetime module
    
    * Add leading underscore to functions
    
    * Fix underscore in function name
    
    * Add flush to stdout
    
    * Add function to flush stream to stdout
    
    * Add flask server
    
    * Update requirements
    
    * Fix naming error
    
    * Add stunnel and remove cert
    
    * Remove retrieve_bytes function
    
    * Add docstring
    
    * Restructure files
    
    * Remove lines from gitignore
    
    * Rename cassandra/ to cass/

---
+++ src/db/cass/src/cluster_services.py
@@ -0,0 +1,48 @@
+import re
+
+class ClusterServices:
+  """The ClusterServices class provides methods used to create the keyspaces and tables 
+     for the Meowl application
+  
+  Attributes:
+      cluster: The Cassandra cluster for this instance
+      session: The current session
+  """
+  def __init__(self, cluster):
+    self.cluster = cluster
+    self.__session = self.cluster.connect()
+
+
+  def get_session(self):
+    return self.__session
+
+
+  def create_keyspace(self, keyspace_name):
+    """Creates a Cassandra keyspace"""
+    create_keyspace_query = "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
+    if self._is_valid_keyspace_name(keyspace_name):
+      self.get_session().execute(create_keyspace_query % (keyspace_name))
+    else:
+      raise Exception("Invalid keyspace name")
+
+
+  def set_keyspace(self, keyspace_name):
+    """Use a Cassandra keyspace"""
+    self.get_session().set_keyspace(keyspace_name);
+
+
+  def create_table_schema(self, table_query):
+    """Creates a table"""
+    self.get_session().execute(table_query);
+
+
+  def shutdown_cluster(self):
+    """Close the cluster when done"""
+    self.cluster.shutdown()
+
+
+  def _is_valid_keyspace_name(self, keyspace_name):
+    """Check if a keyspace name is valid"""
+    if keyspace_name == None or not keyspace_name:
+      return False
+    return re.match(r"^[a-z_]*[^-]$", keyspace_name)
