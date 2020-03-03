73f65b38cf20207519523036fdbd1742311e9dc9
Chieh-Huang Chen <chc9511@gmail.com>
2020-03-03 10:09:07 -0500

    Add mobile development install scripts

---
+++ scripts/lib/install_mobile.sh
@@ -0,0 +1,11 @@
+PARENT_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+
+# Goes into the Meowl mobile directory and installs npm packages
+install_mobile_dev() {
+  echo "Installing Meowl App for Mobile..."
+  cd ${PARENT_PATH}/../../src/camera/mobile
+  npm i
+}
