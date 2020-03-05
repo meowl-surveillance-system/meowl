9bc936fb8ff5ed4d433c93c630bc6d5adfb328fb
MicLi01 <Michael.Li68330@gmail.com>
2020-03-05 11:11:53 -0500

    Facial recognition work

--- src/cv/run_app.sh
+++ src/cv/run_app.sh
@@ -9,6 +9,13 @@ export YOLO_NAMES_PATH=${PARENT}/${YOLO}/${YOLO_NAMES}
 export YOLO_CONFIG_PATH=${PARENT}/${YOLO}/${YOLO_CONFIG}
 export YOLO_WEIGHTS_PATH=${PARENT}/${YOLO}/${YOLO_WEIGHTS}
 
+CAFFE_NAMES='names.txt'
+CAFFE_MODEL='senet50_128.caffemodel'
+CAFFE_PROTOTEXT='senet50_128.prototxt'
+CAFFE='senet50_128_caffe'
+export CAFFE_NAMES_PATH=${PARENT}/'vgg_face_caffe'/${CAFFE_NAMES}
+export CAFFE_MODEL_PATH=${PARENT}/${CAFFE}/${CAFFE_MODEL}
+export CAFFE_PROTOTEXT_PATH=${PARENT}/${CAFFE}/${CAFFE_PROTOTEXT}
 
 # Run Opencv Server
 source venv/bin/activate
