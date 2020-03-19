1b09935f0e8bb92ac142063bef2bab93503e0f4e
Michael Li <37303581+MicLi01@users.noreply.github.com>
2020-03-18 23:52:23 -0400

    Add object detection (#43)
    Setup object detection for live stream processing

---
+++ src/cv/retrieve_model.sh
@@ -0,0 +1,6 @@
+mkdir yolo-coco
+cd yolo-coco
+
+curl -O https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+curl -O https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
+curl -O https://pjreddie.com/media/files/yolov3.weights
