c0b978e9c79b7ac85f210caa29f29547495993ba
melvinheks <mcheria000@citymail.cuny.edu>
2020-03-28 00:48:44 +0000

    Add rtmp authentication to rtmp_saver

--- src/db/cass/app.py
+++ src/db/cass/app.py
@@ -3,9 +3,14 @@ from src.cluster_services import ClusterServices
 from src.file_services import FileServices
 from src.rtmp_saver import RtmpSaver
 from flask import Flask
+from getpass import getpass
 import settings
 
 app = Flask(__name__)
+auth_options = {"loginUrl": settings.LOGIN_URL,
+    "rtmpRequestUrl": settings.RTMP_REQUEST_URL}
+auth_options["username"] = input("Username: ")
+auth_options["password"] = getpass()
 
 cluster_services = ClusterServices(Cluster())
 
@@ -17,35 +22,43 @@ file_services = FileServices(cluster_services.get_session())
 # Handles multiple streams
 running_streams = {}
 
+
 @app.route("/")
 def hello():
   return "Hello World"
 
+
 @app.route("/store/<stream_id>")
 def store_stream(stream_id):
   """Read from stream and store in DB"""
-  try :
-    rtmp_saver = RtmpSaver(settings.RTMP_IP, settings.PORT, stream_id)
-  except :
+  try:
+    rtmp_saver = RtmpSaver(
+      settings.RTMP_IP,
+      settings.PORT,
+      stream_id,
+      auth_options)
+  except BaseException:
     return "Error connecting to stream", 400
-  try :
+  try:
     running_streams[stream_id] = rtmp_saver
     rtmp_saver.start(file_services)
-  except :
+  except BaseException:
     return "Error storing stream", 500
 
   return "Started storing stream", 200
 
+
 @app.route("/stop/<stream_id>")
 def stop_stream(stream_id):
   """Stop reading from stream"""
-  try :
+  try:
     running_streams[stream_id].stop()
     del running_streams[stream_id]
-  except :
+  except BaseException:
     return "Error halting saving of stream", 400
 
   return "Stopped storing stream", 200
+
 
 if __name__ == '__main__':
   app.run(debug=True)
