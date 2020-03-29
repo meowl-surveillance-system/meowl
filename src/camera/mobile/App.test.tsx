097e1f1406448b21208c030d5c5db396316898b7
Chieh-Huang Chen <chc9511@gmail.com>
2020-03-28 23:46:33 -0400

    Fix snapshot bug with asyncstorage

--- src/camera/mobile/App.test.tsx
+++ src/camera/mobile/App.test.tsx
@@ -4,6 +4,9 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
+beforeAll(() => {
+  jest.mock('@react-native-community/async-storage');
+})
 it('renders correctly', () => {
   const container = renderer.create(<App />).toJSON();
   expect(container).toMatchSnapshot();
