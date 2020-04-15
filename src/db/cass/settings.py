d4a4e89e094c042cd7b4791d3c12fb7ed4423914
melvinheks <mcheria000@citymail.cuny.edu>
2020-04-15 22:51:43 +0000

    Replace test-server with test, add env vars to Dockerfile flask app

--- src/db/cass/settings.py
+++ src/db/cass/settings.py
@@ -5,8 +5,8 @@ RTMP_IP = "localhost"
 RTMP_PORT = 1234
 LOGIN_URL = "http://localhost:3000/auth/login"
 RTMP_REQUEST_URL = "http://localhost:3000/auth/rtmpRequest"
-USERNAME = os.environ.get("MEOWL_USERNAME")
 
+USERNAME = os.environ.get("MEOWL_USERNAME")
 if USERNAME is None :
     USERNAME = input("Username: ")
 PASSWORD = os.environ.get("MEOWL_PASSWORD")
