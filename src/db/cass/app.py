1fdb8b54f7e213729b466f80fbbc2ef855c3239c
Chieh-Huang Chen <chc9511@gmail.com>
2020-04-15 16:45:00 -0400

    Create Dockerfiles for cassandra, cassandra-flask-server and nginx-rtmp (#72)

--- src/db/cass/app.py
+++ src/db/cass/app.py
@@ -1,3 +1,4 @@
+from settings import CASSANDRA_CLUSTER_IPS, CASSANDRA_CLUSTER_PORT
 from cassandra.cluster import Cluster
 from src.cluster_services import ClusterServices
 from src.file_services import FileServices
@@ -7,7 +8,8 @@ import settings
 
 app = Flask(__name__)
 
-cluster_services = ClusterServices(Cluster())
+# Connects to local/remote Cluster set by environment variables
+cluster_services = ClusterServices(Cluster(CASSANDRA_CLUSTER_IPS, port=CASSANDRA_CLUSTER_PORT))
 
 # Set the keyspace
 cluster_services.set_keyspace('streams')
--- src/db/cass/settings.py
+++ src/db/cass/settings.py
@@ -1,2 +1,14 @@
+import os
+
 RTMP_IP = "localhost"
 PORT = 1234 
+
+try:
+  CASSANDRA_CLUSTER_IPS = os.environ['CASSANDRA_CLUSTER_IPS'].split(' ')
+except KeyError:
+  raise KeyError('CASSANDRA_CLUSTER_IPS is not defined in environment variable')
+
+try:
+  CASSANDRA_CLUSTER_PORT = int(os.environ['CASSANDRA_CLUSTER_PORT'])
+except KeyError:
+  raise KeyError('CASSANDRA_CLUSTER_PORT is not defined in environment variable')
