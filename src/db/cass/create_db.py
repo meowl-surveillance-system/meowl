999587f3712777bae78aaff44ff223e8fc693695
Michael Li <37303581+MicLi01@users.noreply.github.com>
2020-04-22 19:42:58 -0400

    Automate uploading dataset resources (#76)
    Automate uploading dataset resources

--- src/db/cass/create_db.py
+++ src/db/cass/create_db.py
@@ -38,5 +38,8 @@ cluster_services.create_table_schema('CREATE TABLE IF NOT EXISTS users_name (use
 # Tables for storing OpenCV frames
 cluster_services.create_table_schema('CREATE TABLE IF NOT EXISTS cv_frames (camera_id text, stream_id text, frame_id text, frame blob, objects_detected blob, PRIMARY KEY(frame_id))')
 
+# Table for storing training data
+cluster_services.create_table_schema('CREATE TABLE IF NOT EXISTS training_data (data_id uuid, class_name text, insert_date timestamp, data blob, PRIMARY KEY(data_id))')
+
 # Table for storing Notifications
 cluster_services.create_table_schema('CREATE TABLE IF NOT EXISTS notif (date timestamp, type text, email text, name text, frame_id text, PRIMARY KEY(date))')
