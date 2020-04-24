5e303e4517ea2c6cdf6060f49f6935750d79681b
Chieh-Huang Chen <chc9511@gmail.com>
2020-04-24 19:27:22 -0400

    Fix bug in web

--- src/web/server/app/routes/notif.ts
+++ src/web/server/app/routes/notif.ts
@@ -4,8 +4,9 @@ import * as notifController from '../controllers/notif';
 const app = express();
 
 app.get(
-  '/retrieveNotifications',
-  async (req: express.Request, res: express.Response) => {
-    notifController.retrieveNotifications(req, res);
-  }
-);
+    '/retrieveNotifications',
+    async (req: express.Request, res: express.Response) => {
+      notifController.retrieveNotifications(req, res);
+    });
+
+module.exports = app;
