b903a6594b6fd4219a4db2ac44da6a9a38f6f3bf
Chieh-Huang Chen <chc9511@gmail.com>
2020-04-24 19:32:17 -0400

    fix identation

--- src/web/server/app/routes/notif.ts
+++ src/web/server/app/routes/notif.ts
@@ -4,9 +4,10 @@ import * as notifController from '../controllers/notif';
 const app = express();
 
 app.get(
-    '/retrieveNotifications',
-    async (req: express.Request, res: express.Response) => {
-      notifController.retrieveNotifications(req, res);
-    });
+  '/retrieveNotifications',
+  async (req: express.Request, res: express.Response) => {
+    notifController.retrieveNotifications(req, res);
+  }
+);
 
 module.exports = app;
