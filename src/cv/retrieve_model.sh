b94ba548e7e40014586bfed52c82608ebddf8f66
melvinheks <mcheria000@citymail.cuny.edu>
2020-04-30 14:50:15 +0000

    Fix a couple cv errors

--- src/cv/retrieve_model.sh
+++ src/cv/retrieve_model.sh
@@ -10,4 +10,4 @@ cd ../
 
 # Install unknown dataset into frame_extractions and run initalize_state
 source main.env
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO" -O unknown.rar && rm -rf /tmp/cookies.txt
+wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO" -O unknown.rar && rm -rf /tmp/cookies.txt
