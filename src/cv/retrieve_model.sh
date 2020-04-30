5a0f5f64a061ba96096daad88105f17b4cbe3124
Chieh-Huang Chen <chc9511@gmail.com>
2020-04-30 03:26:04 -0400

    cv fix and web fix

--- src/cv/retrieve_model.sh
+++ src/cv/retrieve_model.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 mkdir yolo-coco dataset output
 
 # Install yolo-coco model
@@ -10,6 +11,3 @@ cd ../
 # Install unknown dataset into frame_extractions and run initalize_state
 source main.env
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ufydwhMYtOhxgQuHs9SjERnkX0fXxorO" -O unknown.rar && rm -rf /tmp/cookies.txt
-unrar e unknown.rar frame_extractions/
-rm unknown.rar
-python3 -c "import insert_frame; insert_frame.store_training_data('unknown')"
---
+++ src/cv/run_setup.sh
@@ -0,0 +1,4 @@
+unrar e unknown.rar frame_extractions/
+rm unknown.rar
+python3 -c "import insert_frame; insert_frame.store_training_data('unknown')"
+
