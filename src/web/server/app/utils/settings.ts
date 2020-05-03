9d39caea34d2c87698a5e66017bac93b2d99b751
Chieh-Huang Chen <chc9511@gmail.com>
2020-05-03 16:28:17 -0400

    Fix env in web dockerfiles

--- src/web/server/app/utils/settings.ts
+++ src/web/server/app/utils/settings.ts
@@ -23,10 +23,10 @@ export const SESSION_SECRET: string =
   process.env.SESSION_SECRET || 'likeasomebooody';
 
 export const NGINX_HLS_SERVER_IP: string =
-  process.env.REACT_APP_NGINX_HLS_SERVER_IP || '127.0.0.1';
+  process.env.NGINX_HLS_SERVER_IP || '127.0.0.1';
 
 export const NGINX_HLS_SERVER_PORT: string =
-  process.env.REACT_APP_NGINX_HLS_SERVER_PORT || '8080';
+  process.env.NGINX_HLS_SERVER_PORT || '8080';
 
 // TODO: Create HTTPS for web server
 export const ENABLE_HTTPS = false;
