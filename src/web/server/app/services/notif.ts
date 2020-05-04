bc6e56695f492977f7f6b66d727549f373a02a6b
Andy Li <43677268+Akasora39@users.noreply.github.com>
2020-05-04 13:06:52 -0400

    Notification fix (#103)
    Change retrieval of frame in DB to be taken using stream id in addition to frame id
    Fix kafka consumer to reflect this change and update table properties in DB

--- src/web/server/app/services/notif.ts
+++ src/web/server/app/services/notif.ts
@@ -15,8 +15,8 @@ export const retrieveNotif = async () => {
  * Retrieve frame associated with notification from database
  * @returns ResultSet - Contains row of frame blob belonging to notification
  */
-export const retrieveFrame = async (frameId: string) => {
-  return client.execute(SELECT_FRAME, [frameId], {
+export const retrieveFrame = async (frame_id: string, stream_id: string) => {
+  return client.execute(SELECT_FRAME, [frame_id, stream_id], {
     prepare: true,
   });
 };
